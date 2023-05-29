6eca95b5ae44cca30ee4dfd34e623b04fcb90401
Helio Tradesis <helio@tradesis.com.br>
2023-05-29 18:10:44 -0300

    ajustes

--- database/crediariocliente.php
+++ database/crediariocliente.php
@@ -15,7 +15,7 @@ function buscaHistoricoCliente($codigoCliente,$cpfCNPJ,$situacao)
 		));
 	
 
-	$retorno = chamaAPI(null, '/crediario/crediario/cliente', json_encode($apiEntrada), 'GET');
+	$retorno = chamaAPI('http://10.2.0.233/bsweb/apitsweb', '/crediario/crediario/cliente', json_encode($apiEntrada), 'GET');
 
    
 	if (isset($retorno["conteudoSaida"])) {
